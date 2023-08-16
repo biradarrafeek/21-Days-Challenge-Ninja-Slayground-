6d15ff453bfcb7dd7b9141b55bff7e2e076846b2
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-08-16 20:57:46 +0530

    Create 1 to N Without Loop .java

---
+++ 1 to N Without Loop .java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static int[] printNos(int x) {
+        // Write Your Code Here
+        int[] arr = new int[x];
+        oneton(arr,x,0);
+        return arr;
+    }
+    public static void oneton(int[] arr,int x,int i){
+        if(i>=x) return;
+        arr[i] = i+1;
+        oneton(arr,x,i+1);
+    }
+}
