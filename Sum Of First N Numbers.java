4561a9e0095be6914341f95f8ee2d2816f300d1c
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-08-17 20:41:07 +0530

    Create Sum Of First N Numbers.java

---
+++ Sum Of First N Numbers.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static int sumFirstN(int n) {
+        // Write Your Code Here
+        // int sum = 0;
+        // for(int i=1;i<=n;i++){
+        //     sum += i;
+        // }
+        //return sum;
+        
+        return n * (n+1)/2;
+    }
+}
