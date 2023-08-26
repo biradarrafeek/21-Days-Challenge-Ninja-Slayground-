9064eb1a32e7f6c918d194ae96125c77049ec83b
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-08-26 20:08:38 +0530

    Create Print Name .java

---
+++ Print Name .java
@@ -0,0 +1,14 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+    static void printName(String s){
+        // Write your code here.
+        int count = 0;
+        while(count<5){
+            System.out.println(s);
+            count++;
+        }
+
+    }
+}
